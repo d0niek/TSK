c87be2700825fbf77bccd64d1f6489412ee8d635
Damian Glinkowski <damianglinkowski@gmail.com>
2016-11-02 22:49:53 +0100

    Move project to cmake (CLion)

--- compile.sh
+++ compile.sh
@@ -6,20 +6,21 @@ wx_flags=$(wx-config --cxxflags)
 wx_libs=$(wx-config --libs --gl-libs)
 
 cpp_files=$(find . -type f -name "*.cpp")
-for file in $cpp_files
-do
-    dir_name=$(dirname $file)
-    file_name=$(basename $file .cpp)
-    if [ ! -d obj/$dir_name ]; then
-        mkdir -p obj/$dir_name
+
+for file in ${cpp_files}; do
+    dir_name=$(dirname ${file})
+    file_name=$(basename ${file} .cpp)
+
+    if [ ! -d obj/${dir_name} ]; then
+        mkdir -p obj/${dir_name}
     fi
 
     echo "Compile file: $dir_name/$file_name.cpp"
 
-    g++ $wx_flags -Wall -g -c $file -o obj/$dir_name/$file_name.o
+    g++ ${wx_flags} -Wall -g -c ${file} -o obj/${dir_name}/${file_name}.o
 done
 
 o_files=$(find obj/ -type f -name "*.o")
 
 mkdir bin
-g++ -o bin/app -lGL -lGLU $o_files $wx_libs
+g++ -o bin/app -lGL -lGLU ${o_files} ${wx_libs}
